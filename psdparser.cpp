11cad009a324080e383bdce0ce8e885946cda89e
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-02-10 10:11:32 +0400

    Добавлен класс парсера psd-файла

---
+++ psdparser.cpp
@@ -0,0 +1,5 @@
+#include "psdparser.h"
+
+PSDParser::PSDParser()
+{
+}
---
+++ psdparser.h
@@ -0,0 +1,10 @@
+#ifndef PSDPARSER_H
+#define PSDPARSER_H
+
+class PSDParser
+{
+public:
+    PSDParser();
+};
+
+#endif // PSDPARSER_H
