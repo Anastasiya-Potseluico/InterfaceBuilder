8fbfa21cccf5ced5c6eb404ffcad7d4f4ed33059
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-02-20 11:56:23 +0400

    Начата реализация функции определения локации
    Локация - местоположение одного контура относительно другого. Может быть
    одной из 5 видов: центр, левый верхний и нижний углы. Аналогично с
    правым углом.

--- geometricalobjectscollector.cpp
+++ geometricalobjectscollector.cpp
@@ -21,14 +21,29 @@ QList<AbstractWidget> GeometricalObjectsCollector::collectObjectsIntoWidgets()
 }
 
 /* Метод для нахождения расположения одного контура в другом*/
-FIGURE_LOCATION GeometricalObjectsCollector::getLocation(QList<std::vector> &figure1, QList<std::vector> &figure2)
+FIGURE_LOCATION GeometricalObjectsCollector::getLocation(QList<std::vector<cv::Point> > &figure1,
+                                                         QList<std::vector<cv::Point> > &figure2)
 {
+    //Найти центр первого контура
+    cv::Moments moment1 = moments(figure1, false);
+    cv::Point2f p1 = cv::Point2f( moment1.m10/moment1.m00 , moment1.m01/moment1.m00 );
+    //Найти центр второго контура
+    cv::Moments moment2 = moments(figure2, false);
+    cv::Point2f p2 = cv::Point2f( moment2.m10/moment2.m00 , moment2.m01/moment2.m00 );
+    //float distance = cv::pointPolygonTest(list.at(i),p,true);
 
+
+    //Найти площади контуров
+    float square1 = cv::contourArea(figure1);
+    float square2 = cv::contourArea(figure2);
+
+    //
 }
 
 /* Метод для выделения кнопок из геометрических объектов*/
 void GeometricalObjectsCollector::findPushButtons()
 {
+
 }
 
 /* Метод для выделения радиокнопок из геометрических объектов*/
