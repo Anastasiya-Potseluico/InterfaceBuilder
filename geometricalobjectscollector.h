fbd4b5528b2523f721b53cbb9b717af7d112f94b
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-03-04 14:51:26 +0400

    Для реализации полиморфизма использ. указатели

--- geometricalobjectscollector.h
+++ geometricalobjectscollector.h
@@ -5,6 +5,7 @@
 #include "opencv2/core/core.hpp"
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "radiobutton.h"
 
 enum FIGURE_LOCATION {left_up, left_down, center_up, center_down, center, left_center, right_center, right_up, right_down, none};
 
@@ -15,13 +16,13 @@ public:
     GeometricalObjectsCollector(QList<std::vector<cv::Point> >&rectangles,
                                 QList<std::vector<cv::Point> >&triangles,
                                 QList<std::vector<cv::Point> >&rounds);
-    QList<AbstractWidget> collectObjectsIntoWidgets();
+    QList<AbstractWidget*> collectObjectsIntoWidgets();
 
 private:
     QList<std::vector<cv::Point> > _rectangles;
     QList<std::vector<cv::Point> > _triangles;
     QList<std::vector<cv::Point> > _rounds;
-    QList<AbstractWidget> _widgets;
+    QList<AbstractWidget*> _widgets;
 
     FIGURE_LOCATION getLocation(std::vector<cv::Point> &figure1, std::vector<cv::Point> &figure2);
     bool isInsideContour(const std::vector<cv::Point> & checkingContour,const std::vector<cv::Point> & contourContainer);
