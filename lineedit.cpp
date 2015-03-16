e94d9024ea87b369f0a89f6c49e5921f6e22d210
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-03-16 20:26:35 +0400

    Добавлен класс лайнэдита

--- lineedit.cpp
+++ lineedit.cpp
@@ -4,6 +4,8 @@
 LineEdit::LineEdit(QPoint &position, int numberOfWidget):AbstractWidget(position,*(new QSize(130,40)))
 {
     _name = QString("LineEdit_").append(QString::number(numberOfWidget));
+    _text = QString("");
+    _readOnly = false;
 }
 
 QString LineEdit::writeSelfIntoFile(QFile &file)
--- lineedit.h
+++ lineedit.h
@@ -8,6 +8,10 @@ public:
     LineEdit(QPoint &position, int numberOfWidget);
     QString writeSelfIntoFile(QFile&file);
     void drawSelf();
+
+private:
+    QString _text; // Содержание лайнэдита.
+    bool _readOnly; // Флаг только на чтение виджета.
 };
 
 #endif // LINEEDIT_H
