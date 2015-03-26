30a27d8a73d9f003be5813f2b190bfebb7148dee
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-03-27 02:05:35 +0400

    Добавлен кодек для поддержки русского языка

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QtGui/QApplication>
 #include <QTest>
+#include <QTextCodec>
 #include "mainwindow.h"
 #include <windows.h>
 #include "testgeometricalobjectcollector.h"
@@ -9,6 +10,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     TestGeometricalObjectCollector test1;
     Sleep( 3000 );
+    QTextCodec *codec = QTextCodec::codecForName("cp1251");
+    QTextCodec::setCodecForCStrings(codec);
     QTest::qExec(&test1);
     MainWindow w;
     w.show();
