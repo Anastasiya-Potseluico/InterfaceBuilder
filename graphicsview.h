58e21e94a382e4523cd07da248bf8a5d23146652
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-03-27 02:05:01 +0400

    Добавлены свойства для виджета отображения графики

--- graphicsview.h
+++ graphicsview.h
@@ -3,12 +3,19 @@
 
 #include "abstractwidget.h"
 
+enum NEEDED_SCROLL_PANELS {always_on, always_off, as_needed};
+
 class GraphicsView : public AbstractWidget
 {
 public:
     GraphicsView(QPoint &position, int numberOfWidget);
     QString writeSelfIntoFile(QFile&file);
     void drawSelf(QGraphicsScene &scene);
+
+private:
+    bool _interactive;
+    NEEDED_SCROLL_PANELS _verticalScrollPanel;
+    NEEDED_SCROLL_PANELS _horizontalScrollPanel;
 };
 
 #endif // GRAPHICSVIEW_H
