d4766baaf39837c510b6547b19f85bea32705b1b
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-03-27 02:06:16 +0400

    Изменено отрисованное представление кнопки

--- pushbuttonview.cpp
+++ pushbuttonview.cpp
@@ -14,8 +14,5 @@ void PushButtonView::paint(QPainter *painter, const QStyleOptionGraphicsItem *op
     painter->drawRect(rect.x(), rect.y(),rect.width(), rect.height());
     //Рисуем внутренний прямоугольник виджета
     painter->setBrush(QBrush(Qt::black));
-    int newX = rect.x() + rect.width()/1.3;
-    int newY = rect.y() + rect.height()/2;
-    QRectF innerRect = QRectF(newX, newY, rect.width()/5,rect.height()/3);
-    painter->drawRect(innerRect);
+    painter->drawText(QPointF(rect.x()+rect.width()/4, rect.y()+rect.height()/1.5),"Кнопка");
 }
