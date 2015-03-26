45fe7c9d58e97eebe025f5208ee71b0d132fc743
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-03-27 02:04:35 +0400

    Добавлено заполнение полей по умолчанию в констр-р

--- graphicsview.cpp
+++ graphicsview.cpp
@@ -2,7 +2,10 @@
 
 GraphicsView::GraphicsView(QPoint &position, int numberOfWidget):AbstractWidget(position,*(new QSize(256,192)))
 {
-
+    _interactive = false;
+    _verticalScrollPanel = as_needed;
+    _horizontalScrollPanel = as_needed;
+    _name = QString("GraphicsView").append(QString::number(numberOfWidget));
 }
 
 void GraphicsView::drawSelf(QGraphicsScene &scene)
