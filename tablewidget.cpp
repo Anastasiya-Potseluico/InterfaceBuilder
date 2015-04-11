c2c41a09be7e32d732f4f5178a90e3266a93a966
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-04-12 01:11:00 +0400

    Добавлена отрисовка виджета таблицы

---
+++ tablewidget.cpp
@@ -0,0 +1,19 @@
+#include "tablewidget.h"
+
+TableWidget::TableWidget(QPoint &position, int numberOfWidget) : AbstractItemWidget(position,*(new QSize(256,192)))
+{
+    _name = QString("TableWidget_").append(QString::number(numberOfWidget));
+    _showGrid = true;
+    _rowCount = 0;
+    _columnCount = 0;
+}
+
+QString TableWidget::writeSelfIntoFile(QFile &file)
+{
+}
+
+void TableWidget::drawSelf(QGraphicsScene &scene)
+{
+    TableWidgetView * view = new TableWidgetView(this);
+    scene.addItem(view);
+}
