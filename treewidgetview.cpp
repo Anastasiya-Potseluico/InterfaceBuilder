25b61b50d75a74bf2df750cc4f538ab2100d7ca0
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-04-12 12:41:11 +0400

    Добавлен класс представления виджета дерева

---
+++ treewidgetview.cpp
@@ -0,0 +1,22 @@
+#include "treewidgetview.h"
+
+TreeWidgetView::TreeWidgetView(AbstractWidget *drawedWidget):AbstractWidgetView(drawedWidget)
+{
+}
+
+void TreeWidgetView::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    QRectF rect = boundingRect();
+    QPen pen(Qt::black, 1);
+
+    //Рисуем внешний прямоугольник виджета
+    painter->setPen(pen);
+    painter->drawRect(rect.x(), rect.y(),rect.width(), rect.height());
+    painter->drawLine(QPointF(rect.x(),rect.y()+rect.height()/4), QPointF(rect.x()+rect.width(),rect.y()+rect.height()/4));
+    QFont font = painter->font();
+    font.setPointSize(12);
+    painter->setFont(font);
+    painter->drawText(QPointF(rect.x()+ 0.4*rect.width(), rect.y()+rect.height()*0.3),"Дерево");
+    painter->drawText(QPointF(rect.x()+ 0.4*rect.width(), rect.y()+rect.height()*0.45),"некоторых");
+    painter->drawText(QPointF(rect.x()+ 0.4*rect.width(), rect.y()+rect.height()*0.6),"данных");
+}
---
+++ treewidgetview.h
@@ -0,0 +1,10 @@
+#ifndef TREEWIDGETVIEW_H
+#define TREEWIDGETVIEW_H
+
+class TreeWidgetView
+{
+public:
+    TreeWidgetView();
+};
+
+#endif // TREEWIDGETVIEW_H
