3c92f675ce3421d853e73519b367b69e56dfb5c9
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-04-16 14:34:29 +0400

    Добавлена реализация метода записи календаря в файл

--- calendar.cpp
+++ calendar.cpp
@@ -11,8 +11,99 @@ Calendar::Calendar(QPoint &position, int numberOfWidget):AbstractWidget(position
     _firstWeekDay = sunday;
 }
 
-QString Calendar::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
+void Calendar::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
 {
+    xmlWriter.writeStartElement("widget");
+    xmlWriter.writeAttribute("class","QCalendarWidget");
+    xmlWriter.writeAttribute("name",_name);
+
+    AbstractWidget::writeSelfIntoFile(xmlWriter);
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","gridVisible");
+    xmlWriter.writeTextElement("bool",_gridVisible ? "true":"false");
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","selectedDate");
+
+    xmlWriter.writeStartElement("date");
+    xmlWriter.writeTextElement("year",QString::number(_selectedDate.year()));
+    xmlWriter.writeTextElement("month",QString::number(_selectedDate.month()));
+    xmlWriter.writeTextElement("day",QString::number(_selectedDate.day()));
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","minimumDate");
+
+    xmlWriter.writeStartElement("date");
+    xmlWriter.writeTextElement("year",QString::number(_minimumDate.year()));
+    xmlWriter.writeTextElement("month",QString::number(_minimumDate.month()));
+    xmlWriter.writeTextElement("day",QString::number(_minimumDate.day()));
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","maximumDate");
+
+    xmlWriter.writeStartElement("date");
+    xmlWriter.writeTextElement("year",QString::number(_maximumDate.year()));
+    xmlWriter.writeTextElement("month",QString::number(_maximumDate.month()));
+    xmlWriter.writeTextElement("day",QString::number(_maximumDate.day()));
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeEndElement();
+
+    QString dayOfWeek = "Qt::";
+
+    switch (_firstWeekDay)
+    {
+        case monday :
+        {
+            dayOfWeek.append("Monday");
+            break;
+        }
+        case tuesday :
+        {
+            dayOfWeek.append("Tuesday");
+            break;
+        }
+        case wednesday:
+        {
+            dayOfWeek.append("Wednesday");
+            break;
+        }
+        case thursday :
+        {
+            dayOfWeek.append("Thursday");
+            break;
+        }
+        case friday :
+        {
+            dayOfWeek.append("Friday");
+            break;
+        }
+        case saturday :
+        {
+            dayOfWeek.append("Saturday");
+            break;
+        }
+        case sunday :
+        {
+            dayOfWeek.append("Sunday");
+            break;
+        }
+    }
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","firstDayOfWeek");
+    xmlWriter.writeTextElement("enum", dayOfWeek);
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeEndElement();
 }
 
 void Calendar::drawSelf(QGraphicsScene &scene)
--- calendar.h
+++ calendar.h
@@ -11,7 +11,7 @@ class Calendar : public AbstractWidget
 {
 public:
     Calendar(QPoint &position, int numberOfWidget);
-    QString writeSelfIntoFile(QXmlStreamWriter &xmlWriter);
+    void writeSelfIntoFile(QXmlStreamWriter &xmlWriter);
     void drawSelf(QGraphicsScene &scene);
 
 private:
