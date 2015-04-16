2d0c43a6485719e76ee6977a3048f774590bfff6
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-04-16 14:00:36 +0400

    Добавлена реализация метода записи абстрактного виджета в файл

--- abstractwidget.cpp
+++ abstractwidget.cpp
@@ -7,20 +7,6 @@ AbstractWidget::AbstractWidget(QPoint &position, QSize &size)
     _enabled = true;
 }
 
-QString AbstractWidget::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
-{
-    int h = 0;
-    xmlWriter.writeStartElement("property");
-    xmlWriter.writeAttribute("name","geometry");
-    xmlWriter.writeStartElement("rect");
-    xmlWriter.writeTextElement("x",QString::number(this->_position.x()));
-    xmlWriter.writeTextElement("y",QString::number(this->_position.y()));
-    xmlWriter.writeTextElement("width",QString::number(this->_size.width()));
-    xmlWriter.writeTextElement("height",QString::number(this->_size.height()));
-    xmlWriter.writeEndElement();
-    xmlWriter.writeEndElement();
-}
-
 QPoint AbstractWidget::getPosition()
 {
     QPoint returnedPoint(_position.x(),_position.y());
--- abstractwidget.h
+++ abstractwidget.h
@@ -5,6 +5,7 @@
 #include <QPoint>
 #include <QString>
 #include <QGraphicsScene>
+#include <QFile>
 #include <QXmlStreamWriter>
 
 
@@ -18,7 +19,25 @@ public:
     QPoint getPosition();
     QSize getSize();
     void setPosition(QPoint &position);
-    QString writeSelfIntoFile(QXmlStreamWriter &xmlWriter);
+    virtual void writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
+    {
+
+            xmlWriter.writeStartElement("property");
+            xmlWriter.writeAttribute("name","geometry");
+            xmlWriter.writeStartElement("rect");
+            xmlWriter.writeTextElement("x",QString::number(this->_position.x()));
+            xmlWriter.writeTextElement("y",QString::number(this->_position.y()));
+            xmlWriter.writeTextElement("width",QString::number(this->_size.width()));
+            xmlWriter.writeTextElement("height",QString::number(this->_size.height()));
+            xmlWriter.writeEndElement();
+            xmlWriter.writeEndElement();
+
+            xmlWriter.writeStartElement("property");
+            xmlWriter.writeAttribute("name","enabled");
+            xmlWriter.writeTextElement("bool",_enabled ? "true":"false");
+            xmlWriter.writeEndElement();
+    }
+
 protected:
     QSize _size; // Размер видета.
     QPoint _position; // Позиция виджета на главное окне.
