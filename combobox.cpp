c825f740ae7a16785bb502eeeb6b07e52a3c59ef
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-04-16 14:47:31 +0400

    Добавлена реализация метода записи комбобокса в файл

--- combobox.cpp
+++ combobox.cpp
@@ -8,9 +8,40 @@ ComboBox::ComboBox (QPoint&position, int numberOfWidget):AbstractWidget(position
     _isEditable = false;
 }
 
-QString ComboBox::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
+void ComboBox::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
 {
+    xmlWriter.writeStartElement("widget");
+    xmlWriter.writeAttribute("class","QComboBox");
+    xmlWriter.writeAttribute("name",_name);
 
+    AbstractWidget::writeSelfIntoFile(xmlWriter);
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","editable");
+    xmlWriter.writeTextElement("bool",_isEditable ? "true":"false");
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","currentIndex");
+    xmlWriter.writeTextElement("number", QString::number(_currentIndex));
+    xmlWriter.writeEndElement();
+
+    if(_items.size()) {
+        int i;
+        for(i = 0; i < _items.size(); i++)
+        {
+            xmlWriter.writeStartElement("item");
+
+            xmlWriter.writeStartElement("property");
+            xmlWriter.writeAttribute("name","text");
+            xmlWriter.writeTextElement("string", _items.at(i));
+            xmlWriter.writeEndElement();
+
+            xmlWriter.writeEndElement();
+        }
+    }
+
+    xmlWriter.writeEndElement();
 }
 
 void ComboBox::drawSelf(QGraphicsScene &scene)
--- combobox.h
+++ combobox.h
@@ -8,7 +8,7 @@ class ComboBox : public AbstractWidget
 {
 public:
     ComboBox(QPoint &position, int numberOfWidget);
-    QString writeSelfIntoFile(QXmlStreamWriter &xmlWriter);
+    void writeSelfIntoFile(QXmlStreamWriter &xmlWriter);
     void drawSelf(QGraphicsScene &scene);
 
 private:
