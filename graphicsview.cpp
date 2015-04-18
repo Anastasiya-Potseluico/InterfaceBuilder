b930d18ee0d4381e630855f18f34ce4aa574d59b
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-04-18 14:31:12 +0400

    Добавлена реализация метода записи в файл для графического виджета

--- graphicsview.cpp
+++ graphicsview.cpp
@@ -14,7 +14,69 @@ void GraphicsView::drawSelf(QGraphicsScene &scene)
     scene.addItem(view);
 }
 
-QString GraphicsView::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
+void GraphicsView::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
 {
+    xmlWriter.writeStartElement("widget");
+    xmlWriter.writeAttribute("class","QGraphicsView");
+    xmlWriter.writeAttribute("name",_name);
+
     AbstractWidget::writeSelfIntoFile(xmlWriter);
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","interactive");
+    xmlWriter.writeTextElement("bool",_interactive ? "true":"false");
+    xmlWriter.writeEndElement();
+
+    QString scrolls;
+
+    switch (_verticalScrollPanel)
+    {
+        case as_needed:
+        {
+            scrolls = "Qt::ScrollBarAsNeeded";
+            break;
+        }
+        case always_on:
+        {
+            scrolls = "Qt::ScrollBarAlwaysOn";
+            break;
+        }
+        case always_off:
+        {
+            scrolls = "Qt::ScrollBarAlwaysOff";
+            break;
+        }
+    }
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","verticalScrollBarPolicy");
+    xmlWriter.writeTextElement("enum",scrolls);
+    xmlWriter.writeEndElement();
+
+    switch (_horizontalScrollPanel)
+    {
+        case as_needed:
+        {
+            scrolls = "Qt::ScrollBarAsNeeded";
+            break;
+        }
+        case always_on:
+        {
+            scrolls = "Qt::ScrollBarAlwaysOn";
+            break;
+        }
+        case always_off:
+        {
+            scrolls = "Qt::ScrollBarAlwaysOff";
+            break;
+        }
+    }
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","horizontalScrollBarPolicy");
+    xmlWriter.writeTextElement("enum",scrolls);
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeEndElement();
+
 }
