2b1ad637f4d662519d3ffcd92a19dd324ba53df3
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-04-18 23:06:53 +0400

    Добавлена реализация метода записи в файл для виджета дерева

--- treewidget.cpp
+++ treewidget.cpp
@@ -7,8 +7,39 @@ TreeWidget::TreeWidget(QPoint &position, int numberOfWidget): AbstractItemWidget
     _columnCount = 1;
 }
 
-QString TreeWidget::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
+void TreeWidget::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
 {
+    xmlWriter.writeStartElement("widget");
+    xmlWriter.writeAttribute("class","QTreeWidget");
+    xmlWriter.writeAttribute("name",_name);
+
+    AbstractItemWidget::writeSelfIntoFile(xmlWriter);
+
+    xmlWriter.writeStartElement("attribute");
+    xmlWriter.writeAttribute("name","headerVisible");
+    xmlWriter.writeTextElement("bool",_headerVisible ? "true":"false");
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","columnCount");
+    xmlWriter.writeTextElement("number",QString::number(_columnCount));
+    xmlWriter.writeEndElement();
+
+    int i;
+    for(i = 0; i < _columnCount; i++)
+    {
+        xmlWriter.writeStartElement("column");
+        xmlWriter.writeStartElement("property");
+        xmlWriter.writeAttribute("name","text");
+        xmlWriter.writeStartElement("string");
+        xmlWriter.writeAttribute("notr","true");
+        xmlWriter.writeCharacters(QString::number(i+1));
+        xmlWriter.writeEndElement();
+        xmlWriter.writeEndElement();
+        xmlWriter.writeEndElement();
+    }
+
+    xmlWriter.writeEndElement();
 }
 
 void TreeWidget::drawSelf(QGraphicsScene &scene)
