3994f0c6c111cf36f0d4ce44237f04be3e057232
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-04-18 22:47:49 +0400

    Добавлена реализация метода записи в файл для виджета таблицы

--- tablewidget.cpp
+++ tablewidget.cpp
@@ -8,8 +8,41 @@ TableWidget::TableWidget(QPoint &position, int numberOfWidget) : AbstractItemWid
     _columnCount = 0;
 }
 
-QString TableWidget::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
+void TableWidget::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
 {
+    xmlWriter.writeStartElement("widget");
+    xmlWriter.writeAttribute("class","QTableWidget");
+    xmlWriter.writeAttribute("name",_name);
+
+    AbstractItemWidget::writeSelfIntoFile(xmlWriter);
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","showGrid");
+    xmlWriter.writeTextElement("bool",_showGrid ? "true":"false");
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","rowCount");
+    xmlWriter.writeTextElement("number",QString::number(_rowCount));
+    xmlWriter.writeEndElement();
+
+    xmlWriter.writeStartElement("property");
+    xmlWriter.writeAttribute("name","columnCount");
+    xmlWriter.writeTextElement("number",QString::number(_columnCount));
+    xmlWriter.writeEndElement();
+
+    int i;
+    for(i = 0; i < _rowCount; i++)
+    {
+        xmlWriter.writeEmptyElement("row");
+    }
+
+    for(i = 0; i < _columnCount; i++)
+    {
+        xmlWriter.writeEmptyElement("column");
+    }
+
+    xmlWriter.writeEndElement();
 }
 
 void TableWidget::drawSelf(QGraphicsScene &scene)
