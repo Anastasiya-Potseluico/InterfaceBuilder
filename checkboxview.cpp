2ec2f700460def9e78581dd4851a94f7843985d0
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-06-09 11:44:49 +0400

    Добавлены докси-комментарии

--- checkboxview.cpp
+++ checkboxview.cpp
@@ -1,24 +1,34 @@
 #include "checkboxview.h"
 
+/*
+* Конструктор представления чекбокса.
+* param [in] drawedWidget Указатель на виджет, который будет отрисовываться.
+*/
 CheckBoxView::CheckBoxView(AbstractWidget *drawedWidget) :AbstractWidgetView(drawedWidget)
 {
 }
 
+/*
+* Переопределение метода отрисовки представления чекбокса.
+* param [in] painter QPainter.
+* param [in] option QStyleOptionGraphicsItem.
+* param [in] widget QWidget.
+*/
 void CheckBoxView::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     QRectF rect = widgetSize();
     QPen pen(Qt::black, 1);
 
-    //Рисуем внешний прямоугольник виджета
+    //Рисуем внешний прямоугольник виджета.
     painter->setPen(pen);
     painter->drawRect(rect.x(), rect.y(),rect.height(), rect.height());
-    //Рисуем внутренний прямоугольник виджета
+    //Рисуем внутренний прямоугольник виджета.
     painter->setBrush(QBrush(Qt::black));
     int newX = rect.x() + rect.height()/4;
     int newY = rect.y() + rect.height()/4;
     QRectF innerRect = QRectF(newX, newY, rect.height()/2,rect.height()/2);
     painter->drawRect(innerRect);
-
+    // Добавляем текст на виджет.
     _itemText.setPos(rect.x()+ 1.1*rect.height(), rect.y()+rect.height()*0.05);
     _itemText.setPlainText("Кнопка-флаг");
     _itemText.setFont(QFont("Cambria", 8));
--- checkboxview.h
+++ checkboxview.h
@@ -3,10 +3,27 @@
 
 #include "abstractwidgetview.h"
 
+/*! \file checkboxview.h
+  *
+  * \class CheckBoxView
+  * \brief Класс представления виджета чекбокса.
+  * \author Поцелуко Анастасия
+*/
 class CheckBoxView : public AbstractWidgetView
 {
 public:
+    /*!
+    *\brief Конструктор представления чекбокса.
+    *\param [in] drawedWidget Указатель на виджет, который будет отрисовываться.
+    */
     CheckBoxView(AbstractWidget *drawedWidget);
+
+    /*!
+    *\brief Переопределение метода отрисовки представления чекбокса.
+    *\param [in] painter QPainter.
+    *\param [in] option QStyleOptionGraphicsItem.
+    *\param [in] widget QWidget.
+    */
     void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget);
 };
 
