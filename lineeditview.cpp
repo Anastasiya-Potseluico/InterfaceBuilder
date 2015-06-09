834d56e0a7e42ab3dbb8a5631ceb03c1517bfbcc
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-06-09 14:21:59 +0400

    Добавлены докси-комментарии

--- lineeditview.cpp
+++ lineeditview.cpp
@@ -1,19 +1,27 @@
 #include "lineeditview.h"
 
-
+/*
+* Конструктор представления строки ввода.
+* param [in] drawedWidget Указатель на виджет, который будет отрисовываться.
+*/
 LineEditView::LineEditView(AbstractWidget *drawedWidget) : AbstractWidgetView(drawedWidget)
 {
 }
 
+/*
+* Переопределение метода отрисовки представления строки ввода.
+* param [in] painter QPainter.
+* param [in] option QStyleOptionGraphicsItem.
+* param [in] widget QWidget.
+*/
 void LineEditView::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     QRectF rect = widgetSize();
     QPen pen(Qt::black, 1);
-
-    //Рисуем внешний прямоугольник виджета
+    // Рисуем внешний прямоугольник виджета.
     painter->setPen(pen);
     painter->drawRect(rect.x(), rect.y(),rect.width(), rect.height());
-
+    // Добавляем надпись на виджет.
     _itemText.setPos(rect.x()+ 0.9*rect.height(), rect.y()+rect.height()*0.05);
     _itemText.setPlainText("Строка ввода");
     _itemText.setFont(QFont("Cambria", 10));
