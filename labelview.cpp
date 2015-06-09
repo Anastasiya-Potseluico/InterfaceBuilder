2a44544cb21946aa943c3ff713a2c9f1814ba4dc
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-06-09 14:21:51 +0400

    Добавлены докси-комментарии

--- labelview.cpp
+++ labelview.cpp
@@ -1,17 +1,27 @@
 #include "labelview.h"
 
+/*
+* Конструктор представления лейбла.
+* param [in] drawedWidget Указатель на виджет, который будет отрисовываться.
+*/
 LabelView::LabelView(AbstractWidget *drawedWidget) : AbstractWidgetView(drawedWidget)
 {
 }
 
+/*
+* Переопределение метода отрисовки представления лейбла.
+* param [in] painter QPainter.
+* param [in] option QStyleOptionGraphicsItem.
+* param [in] widget QWidget.
+*/
 void LabelView::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     QRectF rect = widgetSize();
     QPen pen(Qt::black, 1);
-     painter->setPen(pen);
-    //Добавляем текст "строка ввода"
-     _itemText.setPos(rect.x()+ 0.1*rect.height(), rect.y()+rect.height()*0.03);
-     _itemText.setPlainText("Метка");
-     _itemText.setFont(QFont("Cambria", 10));
-     _itemText.setParentItem(this);
+    painter->setPen(pen);
+    // Добавляем текст для лейбла.
+    _itemText.setPos(rect.x()+ 0.1*rect.height(), rect.y()+rect.height()*0.03);
+    _itemText.setPlainText("Метка");
+    _itemText.setFont(QFont("Cambria", 10));
+    _itemText.setParentItem(this);
 }
--- labelview.h
+++ labelview.h
@@ -3,10 +3,27 @@
 
 #include "abstractwidgetview.h"
 
+/*! \file labelview.h
+  *
+  * \class LabelView
+  * \brief Класс представления лейбла.
+  * \author Поцелуйко Анастасия
+*/
 class LabelView : public AbstractWidgetView
 {
 public:
+    /*!
+    *\brief Конструктор представления лейбла.
+    *\param [in] drawedWidget Указатель на виджет, который будет отрисовываться.
+    */
     LabelView(AbstractWidget *drawedWidget);
+
+    /*!
+    *\brief Переопределение метода отрисовки представления лейбла.
+    *\param [in] painter QPainter.
+    *\param [in] option QStyleOptionGraphicsItem.
+    *\param [in] widget QWidget.
+    */
     void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget);
 };
 
