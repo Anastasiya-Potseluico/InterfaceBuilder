2ac6360f489af23f35d0e3908d089e6a5f32af57
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-06-09 14:21:03 +0400

    Добавлены докси-комментарии

--- graphicsview.cpp
+++ graphicsview.cpp
@@ -1,29 +1,48 @@
 #include "graphicsview.h"
 
+/*
+ * Конструктор графиксвью.
+ * param [in] position Ссылка на позицию, которую должен занять виджет.
+ * param [in] numberOfWidget Порядковый номер виджета.
+ */
 GraphicsView::GraphicsView(QPoint &position, int numberOfWidget):AbstractWidget(position,*(new QSize(256,192)))
 {
+    // Установка флагов по умолчанию.
     _interactive = false;
     _verticalScrollPanel = as_needed;
     _horizontalScrollPanel = as_needed;
     _name = QString("GraphicsView").append(QString::number(numberOfWidget));
+    // Добавление виджетов для настроек.
     addWidgetsForSettings();
 }
 
+/*
+* Метод отрисовки графиксвью.
+* param [in] position Ссылка на сцену, на которой должна производиться отрисовка.
+*/
 void GraphicsView::drawSelf(QGraphicsScene &scene)
 {
+    // Создаем представление графиксвью.
     GraphicsViewView * view = new GraphicsViewView(this);
     scene.addItem(view);
 }
 
+/*
+* Метод установки настроек для графиксвью.
+* param [in] settings Ссылка на карту соответствий настроек и их значений в строковом представлении.
+*/
 void GraphicsView::setSettings(QMap<QString, QString> &settings)
 {
+    // Устанавливаем общие настройки.
     AbstractWidget::setSettings(settings);
     QString temp;
+    // Устанавливаем интерактивность.
     temp = settings.value("interactive");
     if(temp == "true")
         _interactive = true;
     else
         _interactive = false;
+    // Устанавливаем необходимость вертикальной прокрутки.
     temp = settings.value("vertical");
     if(temp == "Always On")
     {
@@ -37,7 +56,7 @@ void GraphicsView::setSettings(QMap<QString, QString> &settings)
     {
         _verticalScrollPanel = as_needed;
     }
-
+    // Устанавливаем необходимость горизонтальной прокрутки.
     temp = settings.value("horizontal");
     if(temp == "Always On")
     {
@@ -55,27 +74,35 @@ void GraphicsView::setSettings(QMap<QString, QString> &settings)
     addWidgetsForSettings();
 }
 
+/*
+* Метод получения имени класса.
+* return Имя класса в виде строки.
+*/
 QString GraphicsView::getClassname()
 {
     return QString("GraphicsView");
 }
 
+/*
+* Метод для добавления нужных виджетов настроек для графиксвью в диалоговом окне.
+*/
 void GraphicsView::addWidgetsForSettings()
 {
+    // Добавляем виджет для редактирования имени графиксвью.
     QLabel *labelName = new QLabel("Name Of Graphics View");
     QLineEdit *name = new QLineEdit();
     name->setObjectName("name");
     name->setText(_name);
     _settings.append(labelName);
     _settings.append(name);
-
+    // Добавляем виджеты для общих настроек.
     AbstractWidget::addWidgetsForSettings();
-
+    // Добавляем флаг интерактивности.
     QCheckBox *interactive = new QCheckBox();
     interactive->setText("Interactive");
     interactive->setChecked(_interactive);
     interactive->setObjectName("interactive");
-
+    // Добавляем комбобокс для настройка вертикальной прокрутки.
     QLabel *labelV= new QLabel("Vertical Scroll Bars");
     QComboBox *vertical = new QComboBox();
     vertical->setObjectName("vertical");
@@ -94,7 +121,7 @@ void GraphicsView::addWidgetsForSettings()
         vertical->setCurrentIndex(2);
         break;
     }
-
+    // Добавляем комбобокс для настройка горизонтальной прокрутки.
     QLabel *labelH= new QLabel("Horizontal Scroll Bars");
     QComboBox *horizontal = new QComboBox();
     horizontal->setObjectName("horizontal");
@@ -121,21 +148,25 @@ void GraphicsView::addWidgetsForSettings()
     _settings.append(horizontal);
 }
 
+/*
+* Метод записи графиксвью в файл.
+* param [in] xmlWriter Ссылка на класс записи в файл в xml формате.
+*/
 void GraphicsView::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
 {
+    // Записываем имя виджета
     xmlWriter.writeStartElement("widget");
     xmlWriter.writeAttribute("class","QGraphicsView");
     xmlWriter.writeAttribute("name",_name);
-
+    // Записываем общие свойства.
     AbstractWidget::writeSelfIntoFile(xmlWriter);
-
+    // Записываем флаг интерактивности.
     xmlWriter.writeStartElement("property");
     xmlWriter.writeAttribute("name","interactive");
     xmlWriter.writeTextElement("bool",_interactive ? "true":"false");
     xmlWriter.writeEndElement();
-
+    // Записываем свойство вертикальной прокрутки.
     QString scrolls;
-
     switch (_verticalScrollPanel)
     {
         case as_needed:
@@ -154,12 +185,11 @@ void GraphicsView::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
             break;
         }
     }
-
     xmlWriter.writeStartElement("property");
     xmlWriter.writeAttribute("name","verticalScrollBarPolicy");
     xmlWriter.writeTextElement("enum",scrolls);
     xmlWriter.writeEndElement();
-
+    // Записываем свойство горизонтальной прокрутки.
     switch (_horizontalScrollPanel)
     {
         case as_needed:
@@ -178,12 +208,10 @@ void GraphicsView::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
             break;
         }
     }
-
     xmlWriter.writeStartElement("property");
     xmlWriter.writeAttribute("name","horizontalScrollBarPolicy");
     xmlWriter.writeTextElement("enum",scrolls);
     xmlWriter.writeEndElement();
 
     xmlWriter.writeEndElement();
-
 }
--- graphicsview.h
+++ graphicsview.h
@@ -5,19 +5,57 @@
 
 enum NEEDED_SCROLL_PANELS {always_on, always_off, as_needed};
 
+/*! \file graphicsview.h
+  *
+  * \class GraphicsView
+  * \brief Класс виджета графического отображения.
+  * \author Поцелуйко Анастасия
+*/
 class GraphicsView : public AbstractWidget
 {
 public:
+   /*!
+    *\brief Конструктор графиксвью.
+    *\param [in] position Ссылка на позицию, которую должен занять виджет.
+    *\param [in] numberOfWidget Порядковый номер виджета.
+    */
     GraphicsView(QPoint &position, int numberOfWidget);
+
+    /*!
+    *\brief Метод записи графиксвью в файл.
+    *\param [in] xmlWriter Ссылка на класс записи в файл в xml формате.
+    */
     void writeSelfIntoFile(QXmlStreamWriter &xmlWriter);
+
+    /*!
+    *\brief Метод отрисовки графиксвью.
+    *\param [in] position Ссылка на сцену, на которой должна производиться отрисовка.
+    */
     void drawSelf(QGraphicsScene &scene);
+
+    /*!
+    *\brief Метод установки настроек для графиксвью.
+    *\param [in] settings Ссылка на карту соответствий настроек и их значений в строковом представлении.
+    */
     void setSettings(QMap<QString, QString> &settings);
+
+    /*!
+    *\brief Метод получения имени класса.
+    *\return Имя класса в виде строки.
+    */
     QString getClassname();
+
+    /*!
+    *\brief Метод для добавления нужных виджетов настроек для графиксвью в диалоговом окне.
+    */
     void addWidgetsForSettings();
 
 private:
+    /*!\brief Флаг интерактивности*/
     bool _interactive;
+    /*!\brief Необходимость вертикальной прокрутки*/
     NEEDED_SCROLL_PANELS _verticalScrollPanel;
+    /*!\brief Необходимость горизонтальной прокрутки*/
     NEEDED_SCROLL_PANELS _horizontalScrollPanel;
 };
 
