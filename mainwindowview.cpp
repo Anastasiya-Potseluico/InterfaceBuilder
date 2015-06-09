9ae8e84ec7df58f0a9f47b5ab7f8e232e91f62cd
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-06-09 16:37:16 +0400

    Добавлены докси-комментарии

--- mainwindowview.cpp
+++ mainwindowview.cpp
@@ -1,15 +1,25 @@
 #include "mainwindowview.h"
 
+/*
+* Конструктор представления главного окна.
+* param [in] drawedWidget Указатель на виджет, который будет отрисовываться.
+*/
 MainWindowView::MainWindowView(AbstractWidget *drawedWidget):AbstractWidgetView(drawedWidget)
 {
     this->setFlag(QGraphicsItem::ItemIsMovable,false);
 }
 
+/*
+* Переопределение метода отрисовки представления главного окна.
+* param [in] painter QPainter.
+* param [in] option QStyleOptionGraphicsItem.
+* param [in] widget QWidget.
+*/
 void MainWindowView::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     QRectF rect = widgetSize();
     QPen pen(Qt::black, 1);
-    //Рисуем внешний прямоугольник виджета
+    // Рисуем внешний прямоугольник виджета.
     painter->setPen(pen);
     painter->drawRect(rect.x(), rect.y(),rect.width(), rect.height());
 }
--- mainwindowview.h
+++ mainwindowview.h
@@ -3,10 +3,27 @@
 
 #include "abstractwidgetview.h"
 
+/*! \file miainwindowview.h
+  *
+  * \class MainWindowView
+  * \brief Класс представления главного окна.
+  * \author Поцелуйко Анастасия
+*/
 class MainWindowView : public AbstractWidgetView
 {
 public:
+    /*!
+    *\brief Конструктор представления главного окна.
+    *\param [in] drawedWidget Указатель на виджет, который будет отрисовываться.
+    */
     MainWindowView(AbstractWidget *drawedWidget);
+
+    /*!
+    *\brief Переопределение метода отрисовки представления главного окна.
+    *\param [in] painter QPainter.
+    *\param [in] option QStyleOptionGraphicsItem.
+    *\param [in] widget QWidget.
+    */
     void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget);
 };
 
