af5c2f0925b1f0a40b7ca8e67d9b71de95c5c1f9
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-06-09 16:36:53 +0400

    Добавлены докси-комментарии

--- mainwindowcontainer.cpp
+++ mainwindowcontainer.cpp
@@ -1,31 +1,39 @@
 #include "mainwindowcontainer.h"
 
+/*
+ * Конструктор виджета главного окна.
+ */
 MainWindowContainer::MainWindowContainer():AbstractWidget(*(new QPoint(0,0)),*(new QSize(256,192)))
 {
+    // Заполнение полей по умолчанию.
     _windowTitle = "MainWindow";
     _name = "MainWindow";
-    addWidgetsForSettings();
     _autoLayout = false;
+    addWidgetsForSettings();
 }
 
+/*
+* Метод записи главного окна в файл.
+* param [in] xmlWriter Ссылка на класс записи в файл в xml формате.
+*/
 void MainWindowContainer::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
 {
+    // Записать название главного окна.
     xmlWriter.writeTextElement("class",_name);
     xmlWriter.writeStartElement("widget");
     xmlWriter.writeAttribute("class","QMainWindow");
     xmlWriter.writeAttribute("name",_name);
     AbstractWidget::writeSelfIntoFile(xmlWriter);
-
+    // Записать заголовок окна.
     xmlWriter.writeStartElement("property");
     xmlWriter.writeAttribute("name","windowTitle");
     xmlWriter.writeTextElement("string",_windowTitle);
     xmlWriter.writeEndElement();
-
-
+    // Записать центральный виджет.
     xmlWriter.writeStartElement("widget");
     xmlWriter.writeAttribute("class","QWidget");
     xmlWriter.writeAttribute("name","centralWidget");
-
+    // Если есть компоновка, предусмотреть ее запись.
     if(_autoLayout)
     {
         xmlWriter.writeStartElement("layout");
@@ -33,6 +41,7 @@ void MainWindowContainer::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
         xmlWriter.writeAttribute("name","gridLayout");
         setWidgetsIntoGrig();
     }
+    // Записать каждый виджет в файл.
     int i;
     for (i = 0; i < _widgets.size(); i++) {
 
@@ -51,7 +60,7 @@ void MainWindowContainer::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
 
         }
     }
-    //записать каждый виджет в файл
+    // Закрыть тег компоновки, если она была выставлена.
     if(_autoLayout)
     {
         xmlWriter.writeEndElement();
@@ -61,10 +70,16 @@ void MainWindowContainer::writeSelfIntoFile(QXmlStreamWriter &xmlWriter)
     xmlWriter.writeEndElement();
 }
 
+/*
+* Метод отрисовки главного окна.
+* param [in] position Ссылка на сцену, на которой должна производиться отрисовка.
+*/
 void MainWindowContainer::drawSelf(QGraphicsScene &scene)
 {
+    // Создать представление главного окна.
     MainWindowView * mainWindow= new MainWindowView(this);
     scene.addItem(mainWindow);
+    // Отрисовать каждый виж, принадлежащий главному окну.
     int i;
     for (i = 0; i < _widgets.size(); i++)
     {
@@ -72,67 +87,98 @@ void MainWindowContainer::drawSelf(QGraphicsScene &scene)
     }
 }
 
-void MainWindowContainer::setWidgets(QList<AbstractWidget *> widgets)
+/*
+* Метод добавления списка виджетов на главное окно.
+* param [in] position Ссылка на список виджетов.
+*/
+void MainWindowContainer::setWidgets(QList<AbstractWidget *> &widgets)
 {
     if(widgets.size()!=0)
     {
         _widgets = widgets;
+        // Посчитать размер главного окна.
         _size = countWindowDimension();
     }
 }
 
+/*
+* Метод установки настроек для главного окна.
+* param [in] settings Ссылка на карту соответствий настроек и их значений в строковом представлении.
+*/
 void MainWindowContainer::setSettings(QMap<QString, QString> &settings)
 {
+    // Установить общие настройки.
     AbstractWidget::setSettings(settings);
+    // Установить заголовок главного окна.
     QString tempString = settings.value("windowTitle");
     _windowTitle = tempString;
     _settings.clear();
     addWidgetsForSettings();
 }
 
+/*
+* Метод получения имени класса.
+* return Имя класса в виде строки.
+*/
 QString MainWindowContainer::getClassname()
 {
     return QString("MainWindow");
 }
 
+/*
+* Метод для добавления нужных виджетов настроек для главного окна в диалоговом окне.
+*/
 void MainWindowContainer::addWidgetsForSettings()
 {
-    _settings.clear();
+    // Добавить виджет для редактирования имени главного окна.
     QLabel *labelName = new QLabel("Name Of Main Window");
     QLineEdit *name = new QLineEdit();
     name->setObjectName("name");
     name->setText(_name);
     _settings.append(labelName);
     _settings.append(name);
-
+    // Добавить виджеты для редактирования общих свойств.
     AbstractWidget::addWidgetsForSettings();
-
+    // Добавить виджет для изменения заголовка окна.
     QLabel *labelTitle = new QLabel("Window Title");
     QLineEdit *title = new QLineEdit();
     title->setObjectName("windowTitle");
     title->setText(_windowTitle);
-
     _settings.append(labelName);
     _settings.append(name);
     _settings.append(labelTitle);
     _settings.append(title);
 }
 
+/*
+* Метод установки лейаута для главного окна.
+* param [in] layout Значение флага установки компоновки.
+*/
 void MainWindowContainer::setAutoLayout(bool layout)
 {
     _autoLayout = layout;
 }
 
+/*
+* Метод получения виджетов на главном окне.
+* return Список виджетов главного окна.
+*/
 QList<AbstractWidget *> MainWindowContainer::getWidgets()
 {
     return _widgets;
 }
 
+/*
+* Метод получения размера главного окна в зависимости от виджетов.
+* return Подсчитанный размер главного окна.
+*/
 QSize MainWindowContainer::countWindowDimension()
 {
     int maxY;
     int maxX;
+    int border = 30;
     int i;
+    // Высчитать максимальные координаты по осям х и у.
     maxY = _widgets[0]->getPosition().y() + _widgets[0]->getSize().height();
     maxX = _widgets[0]->getPosition().x() + _widgets[0]->getSize().width();
     for(i = 0; i < _widgets.size(); i++)
@@ -146,9 +192,13 @@ QSize MainWindowContainer::countWindowDimension()
             maxY = _widgets[i]->getPosition().y() + _widgets[i]->getSize().height();
         }
     }
-    return QSize(maxX+30, maxY+30);
+    // Сделать отступ в размере переменной border.
+    return QSize(maxX+border, maxY+border);
 }
 
+/*
+* Метод для распределения виджетов по сетке компоновки.
+*/
 void MainWindowContainer::setWidgetsIntoGrig()
 {
     int minWidth; // Минимальная ширина виджета.
@@ -199,16 +249,23 @@ void MainWindowContainer::setWidgetsIntoGrig()
     }
 }
 
+/*
+* Метод получения номера ячейки сетки по одному из параметров.
+* param [in] widgetAttribute Одна из координат, ширина или высота виджета.
+* param [in] cellDimension Ширина или высота ячейки сетки.
+* return Номер ячейки сетки по вертикали или горизонтали.
+*/
 int MainWindowContainer::getCellsCount(int widgetAttribute, int cellDimension)
 {
+    // Ищем целый номер ячейки.
     int dimension = widgetAttribute/cellDimension;
     float div = float (widgetAttribute % cellDimension);
+    // Ищем остаток деления параметра на шаг сетки.
     float res = div / ((float)cellDimension);
-
+    // Нужно определить, в какую сторону округлить количество ячеек.
     if( res >= 0.5)
     {
         dimension += 1;
     }
-
     return dimension;
 }
--- mainwindowcontainer.h
+++ mainwindowcontainer.h
@@ -4,26 +4,93 @@
 #include "abstractwidget.h"
 #include "mainwindowview.h"
 
+/*! \file mainwindowcontainer.h
+  *
+  * \class MainWindowContainer
+  * \brief Класс виджета главного окна.
+  * \author Поцелуйко Анастасия
+*/
 class MainWindowContainer : public AbstractWidget
 {
 public:
+    /*!
+     *\brief Конструктор виджета главного окна.
+     */
     MainWindowContainer();
+
+    /*!
+    *\brief Метод записи главного окна в файл.
+    *\param [in] xmlWriter Ссылка на класс записи в файл в xml формате.
+    */
     void writeSelfIntoFile(QXmlStreamWriter &xmlWriter);
+
+    /*!
+    *\brief Метод отрисовки главного окна.
+    *\param [in] position Ссылка на сцену, на которой должна производиться отрисовка.
+    */
     void drawSelf(QGraphicsScene &scene);
-    void setWidgets(QList<AbstractWidget*> widgets);
+
+    /*!
+    *\brief Метод добавления списка виджетов на главное окно.
+    *\param [in] widgets Ссылка на список виджетов.
+    */
+    void setWidgets(QList<AbstractWidget*> &widgets);
+
+    /*!
+    *\brief Метод установки настроек для главного окна.
+    *\param [in] settings Ссылка на карту соответствий настроек и их значений в строковом представлении.
+    */
     void setSettings(QMap<QString, QString> &settings);
+
+    /*!
+    *\brief Метод получения имени класса.
+    *\return Имя класса в виде строки.
+    */
     QString getClassname();
+
+    /*!
+    *\brief Метод для добавления нужных виджетов настроек для главного окна в диалоговом окне.
+    */
     void addWidgetsForSettings();
+
+    /*!
+    *\brief Метод установки лейаута для главного окна.
+    *\param [in] layout Значение флага установки компоновки.
+    */
     void setAutoLayout(bool layout);
+
+    /*!
+    *\brief Метод получения виджетов на главном окне.
+    *\return Список виджетов главного окна.
+    */
     QList<AbstractWidget*> getWidgets();
 
 private:
-    QList <AbstractWidget*> _widgets; // Список виджетов, расположенных на главном окне.
-    QString _windowTitle; // Название главного окна.
-    QSize countWindowDimension(); // Метод расчета размера окна на основе имеющихся виджетов.
-    void setWidgetsIntoGrig();
-    int getCellsCount(int widgetAttribute, int cellDimension);
+    /*!\brief Список виджетов, расположенных на главном окне*/
+    QList <AbstractWidget*> _widgets;
+    /*!\brief Заголовок главного окна*/
+    QString _windowTitle;
+    /*!\brief Флаг автокомпоновки*/
     bool _autoLayout;
+
+    /*!
+    *\brief Метод получения размера главного окна в зависимости от виджетов.
+    *\return Подсчитанный размер главного окна.
+    */
+    QSize countWindowDimension();
+
+    /*!
+    *\brief Метод для распределения виджетов по сетке компоновки.
+    */
+    void setWidgetsIntoGrig();
+
+    /*!
+    *\brief Метод получения номера ячейки сетки по одному из параметров.
+    *\param [in] widgetAttribute Одна из координат, ширина или высота виджета.
+    *\param [in] cellDimension Ширина или высота ячейки сетки.
+    *\return Номер ячейки сетки по вертикали или горизонтали.
+    */
+    int getCellsCount(int widgetAttribute, int cellDimension);
 };
 
 #endif // MAINWINDOWCONTAINER_H
