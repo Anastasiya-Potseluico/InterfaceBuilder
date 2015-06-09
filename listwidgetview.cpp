032b1ea87bcdeeeea7c9b152b83a57406a36f293
Anastasiya-Potseluico <anastasiyasergeevnap@gmail.com>
2015-06-09 16:37:02 +0400

    Добавлены докси-комментарии

--- listwidgetview.cpp
+++ listwidgetview.cpp
@@ -1,16 +1,27 @@
 #include "listwidgetview.h"
 
+/*
+* Конструктор представления списка.
+* param [in] drawedWidget Указатель на виджет, который будет отрисовываться.
+*/
 ListWidgetView::ListWidgetView(AbstractWidget *drawedWidget) :AbstractWidgetView(drawedWidget)
 {
 }
 
+/*
+* Переопределение метода отрисовки представления списка.
+* param [in] painter QPainter.
+* param [in] option QStyleOptionGraphicsItem.
+* param [in] widget QWidget.
+*/
 void ListWidgetView::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
     QRectF rect = widgetSize();
     QPen pen(Qt::black, 1);
-    //Рисуем внешний прямоугольник виджета
+    // Рисуем внешний прямоугольник виджета.
     painter->setPen(pen);
     painter->drawRect(rect.x(), rect.y(),rect.width(), rect.height());
+    // Добавляем текст на список.
     _itemText.setPos(rect.x()+ 0.2*rect.width(), rect.y()+rect.height()*0.2);
     _itemText.setPlainText("Список \nнекоторых \nданных");
     _itemText.setFont(QFont("Cambria", 12));
--- listwidgetview.h
+++ listwidgetview.h
@@ -3,10 +3,27 @@
 
 #include "abstractwidgetview.h"
 
+/*! \file listwidgetview.h
+  *
+  * \class ListWidgetView
+  * \brief Класс представления виджета списка.
+  * \author Поцелуйко Анастасия
+*/
 class ListWidgetView : public AbstractWidgetView
 {
 public:
+    /*!
+    *\brief Конструктор представления списка.
+    *\param [in] drawedWidget Указатель на виджет, который будет отрисовываться.
+    */
     ListWidgetView(AbstractWidget *drawedWidget);
+
+    /*!
+    *\brief Переопределение метода отрисовки представления списка.
+    *\param [in] painter QPainter.
+    *\param [in] option QStyleOptionGraphicsItem.
+    *\param [in] widget QWidget.
+    */
     void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget);
 };
 #endif // LISTWIDGETVIEW_H
